cf84d835c1b9ab317295395c9e657334df66345c
Song Jongbeen <1041489@gmail.com>
2021-09-28 19:14:21 +0900

    fix lstrip to replace
    fixed following error:
    1. t, a 등의 알파벳이 첫 문자로 올 때 strip되어버리는 현상

--- Component/Register/Register.py
+++ Component/Register/Register.py
@@ -8,7 +8,7 @@ from ..Util.Excel import worksheet_write, create_worksheet, save_excel_file
 def register_exam():
     exam_folder = create_folder('./data', '모의고사')
     if exam_folder:
-        exam_excel_path = exam_folder + f'/{exam_folder.lstrip("./data")} 정답 및 배점.xlsx'
+        exam_excel_path = exam_folder + f'{exam_folder.replace("./data", "")} 정답 및 배점.xlsx'
         if not os.path.exists(exam_excel_path):
             create_exam_excel(exam_folder)
         open_file(exam_excel_path)
@@ -21,13 +21,13 @@ def create_exam_excel(path):
     for question_number in range(2, 22):
         worksheet_write(worksheet, question_number, question_number - 1)
 
-    return save_excel_file(workbook, path, f'{path.lstrip("./data")} 정답 및 배점')
+    return save_excel_file(workbook, path, f'{path.replace("./data", "")} 정답 및 배점')
 
 
 # 모의고사 폴더를 선택하여 학생들의 제출 답안 엑셀 파일을 생성하고, 파일을 엽니다.
 def register_students_submission():
     exam_folder = select_exam_folder()
-    submission_file = exam_folder + f'/{exam_folder.lstrip("./data")} 학생 제출 답.xlsx'
+    submission_file = exam_folder + f'{exam_folder.replace("./data", "")} 학생 제출 답.xlsx'
     if not os.path.exists(submission_file):
         create_students_submission_excel(exam_folder)
 
@@ -39,4 +39,4 @@ def create_students_submission_excel(exam_path):
     workbook, worksheet = create_worksheet('제출답안')
     worksheet_write(worksheet, 1, '학생 이름', '지점명', '제출답안')
 
-    return save_excel_file(workbook, exam_path, f'{exam_path.lstrip("./data")} 학생 제출 답')
+    return save_excel_file(workbook, exam_path, f'{exam_path.replace("./data", "")} 학생 제출 답')
